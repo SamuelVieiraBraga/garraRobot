8a3da2fbab7b9681e57e42aa40e4b491e861fc91
SamuelVieiraBraga <108405679+SamuelVieiraBraga@users.noreply.github.com>
2022-06-29 11:34:10 -0300

    Add files via upload

---
+++ Garra_come.ino
@@ -0,0 +1,36 @@
+#include <Ultrasonic.h>
+#include <Servo.h>
+  Servo Base;
+  Servo FT;
+  Servo CB;
+  Servo Boca;
+  int Ab = 0;
+  
+void setup() {
+ // Base.attach(3);
+ // FT.attach(5);
+// CB.attach(11);
+  Boca.attach(9);
+}
+
+void loop() {
+/*
+ for (Ab = 0; Ab <= 0; Ab += 1) {
+  Base.write(Ab);
+  delay(20);
+ }
+ for (Ab = 0; Ab >= 0; Ab -= 1) {
+ Base.write(Ab);
+ delay(20);
+//Faz com que a realize uma transição do ponto 0 ao 180 e do 180 ao 0
+ */
+  //Ultrassom teste
+
+  
+  
+
+
+
+ 
+ }
+}
